34eebbd833f7f6b8ed9b1a425e7cbed69c45eb6e
NicolasChaparro777 <jchaparroro@unal.edu.co>
2024-11-07 21:28:34 -0500

    Correciones 1
    Corregí los errores que tenía en el método de la clase Auto verificarIntegridad()

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -37,6 +37,7 @@ public class Auto {
                 }
                 else{
                     comprobante = false;
+                    break;
                 }
                 }
             }
